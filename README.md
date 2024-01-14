96a71f8dda073096c45eeece992ef1ce0843be3b
Continuous index <continuousindex@gmail.com>
2024-01-14 09:58:13 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# TailwindCSS-offline
+ All the Tailwind CSS v3.4.1 basic classes in one css file to work offline
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{php,html,js}"],
+  corePlugins: { preflight: true },
+  theme: {
+    extend: {},
+  }
+}
